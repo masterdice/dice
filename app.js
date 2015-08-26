6e8b27e3fc835994fe0acb91d48f435666e18e7c
investdice <yeskt2@gmail.com>
2015-08-26 21:23:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -113,9 +113,9 @@ helpers.roleToLabelElement = function(role) {
     case 'ADMIN':
       return el.span({className: 'label label-danger'}, 'MP Staff');
     case 'MOD':
-      return el.span({className: 'label label-info'}, '☆Mod');
+      return el.span({className: 'label label-info'}, '☆Mod☆');
     case 'OWNER':
-      return el.span({className: 'label label-primary'}, '★Owner');
+      return el.span({className: 'label label-primary'}, '★Owner★');
     default:
       return '';
   }
